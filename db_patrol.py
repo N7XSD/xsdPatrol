83109d6f62831301e9b507e31fd78093f8bc026d
Joseph Scanlan <joseph@n7xsd.us>
2026-08-13 12:18:45 -0700

    The connection and cursor objects are now local to PatrolDB
    They are also not exposed to other objects.

--- db_patrol.py
+++ db_patrol.py
@@ -19,6 +19,9 @@ class PatrolDB():
     # datetime objects.  They must accept date time as datetime
     # objects and convert to the database native format.
 
+    conn = None
+    curs = None
+
     def __init__(self, cmn):
         logging.debug("Init db_patrol.PatrolDB")
         self.cmn = cmn
@@ -68,36 +71,34 @@ class PatrolDB():
                 vl = i.values()
                 vl.insert(0, member.member_id)
                 note_lists.append(vl)
-        conn = None
-        curs = None
         try:
-            conn = self.db_connect()
-            conn.begin()
-            curs = self.db_cursor(conn)
-# The following lines are tested but commented out for development
-#           curs.execute(sql_statement, sql_values)
-#           self.add_member_telephone(telephone_lists, db_cursor=curs)
-#           self.add_member_email(email_lists, db_cursor=curs)
-#           self.add_member_phys_addr(phys_addr_lists, db_cursor=curs)
-#           self.add_member_note(note_lists, db_cursor=curs)
-            conn.commit()
-            curs.close()
-            curs = None
-            conn.close()
-            conn = None
+            self.db_connect()
+            self.conn.begin()
+            self.db_cursor()
+            print("Um... adding data to tables disabled right now")
+# The following lines are commented out for development
+#           self.curs.execute(sql_statement, sql_values)
+#           self.add_member_telephone(telephone_lists)
+#           self.add_member_email(email_lists)
+#           self.add_member_phys_addr(phys_addr_lists)
+#           self.add_member_note(note_lists)
+            self.conn.commit()
+            self.curs.close()
+            self.curs = None
+            self.conn.close()
+            self.conn = None
         except mariadb.Error as e:
             print(type(e), e)
             ## FIXME: raise something or other
-        if curs is not None:
-            curs.close()
-        if conn is not None:
-            conn.close()
+        if self.curs is not None:
+            self.curs.close()
+        if self.conn is not None:
+            self.conn.close()
 
-    def add_member_email(self, list_list, db_cursor=None):
+    def add_member_email(self, list_list):
         """Take a list of EmailAddress objects for a member and add them
            to the database"""
 
-        curs = db_cursor
         sql_statement = """
             INSERT INTO email_address (member_id, active, email_date,
                 email_type, email_addr)
@@ -114,16 +115,15 @@ class PatrolDB():
 #       print(sql_values)
 #       print()
         try:
-            curs.execute(sql_statement, sql_values)
+            self.curs.execute(sql_statement, sql_values)
         except mariadb.Error as e:
             print(type(e), e)
             ## FIXME: raise something or other
 
-    def add_member_note(self, list_list, db_cursor=None):
+    def add_member_note(self, list_list):
         """Take a list of MemberNote objects for a member and add them
            to the database"""
 
-        curs = db_cursor
         sql_statement = """
             INSERT INTO member_note (member_id, active, note_time,
                 member_note)
@@ -140,16 +140,15 @@ class PatrolDB():
 #       print(sql_values)
 #       print()
         try:
-            curs.execute(sql_statement, sql_values)
+            self.curs.execute(sql_statement, sql_values)
         except mariadb.Error as e:
             print(type(e), e)
             ## FIXME: raise something or other
 
-    def add_member_phys_addr(self, list_list, db_cursor=None):
+    def add_member_phys_addr(self, list_list):
         """Take a list of PhysicalAddress objects for a member and add
            them to the database"""
 
-        curs = db_cursor
         sql_statement = """
             INSERT INTO physical_address (member_id, active,
                 phys_addr_date, phys_addr_type, country_code,
@@ -169,16 +168,15 @@ class PatrolDB():
 #       print(sql_values)
 #       print()
         try:
-            curs.execute(sql_statement, sql_values)
+            self.curs.execute(sql_statement, sql_values)
         except mariadb.Error as e:
             print(type(e), e)
             ## FIXME: raise something or other
 
-    def add_member_telephone(self, list_list, db_cursor=None):
+    def add_member_telephone(self, list_list):
         """Take a list of TelephoneNumber objects for a member and add
            them to the database"""
 
-        curs = db_cursor
         sql_statement = """
             INSERT INTO telephone_number (member_id, active, phone_date,
                 phone_type, phone_country_code, phone_number, phone_ext)
@@ -195,7 +193,7 @@ class PatrolDB():
 #       print(sql_values)
 #       print()
         try:
-            curs.execute(sql_statement, sql_values)
+            self.curs.execute(sql_statement, sql_values)
         except mariadb.Error as e:
             print(type(e), e)
             ## FIXME: raise something or other
@@ -218,7 +216,7 @@ class PatrolDB():
         try:
             logging.info(f"Attempting connection to {self.db_database}")
             # Instantiate Connection
-            conn = mariadb.connect(
+            self.conn = mariadb.connect(
                 host=self.db_host,
                 port=self.db_port,
                 ssl_verify_cert=True, # FIXME
@@ -228,35 +226,42 @@ class PatrolDB():
             logging.info("Connected")
         except mariadb.Error as e:
             logging.info(f"Error connecting to the database: {e}")
-            conn = None
-        return conn
+            self.conn = None
 
-    def db_cursor(self, conn):
-        if conn:
+    def db_cursor(self):
+        if self.conn:
             try:
-                curs = conn.cursor(named_tuple=True)
+                self.curs = self.conn.cursor(named_tuple=True)
             except mariadb.Error as e:
                 logging.info(f"MariaDB Error creating database curesor: {e}")
-                curs = None
+                self.curs = None
             except:
                 logging.info(f"Error creating database curesor: {e}")
-                curs = None
-        return curs
+                self.curs = None
+
+    def db_table_list(self):
+        """Return a list of table names"""
+
+        self.db_connect()
+        items = []
+        if self.conn:
+            self.db_cursor()
+            if self.curs:
+                self.curs.execute("SHOW TABLES")
+                for i in self.curs:
+                    items.append(i)
+                self.curs.close()
+                self.conn.close()
+        self.conn = None
+        return items
 
 if __name__ == '__main__':
     cmn = common.Common()
     pdb = PatrolDB(cmn)
-    conn = pdb.db_connect()
-    if conn:
-        curs = pdb.db_cursor(conn)
-        if curs:
-            print(f"### Tables:")
-            curs.execute("SHOW TABLES")
-            for i in curs:
-                print(f"    {i[0]}")
-            curs.close()
-            conn.close()
-        else:
-            print("PatrolDB error: Cursor not defined")
+    tables = pdb.db_table_list()
+    if tables:
+        print(f"### Tables:")
+        for i in tables:
+            print(f"    {i[0]}")
     else:
-        print("PatrolDB error: Connection not defined")
+       print("No table names found")
